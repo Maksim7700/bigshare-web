4c6d27278882163687c8d595848660ee83f7cea9
Maksym Kuziv <mkuziv@hubspot.com>
2022-11-10 23:38:22 +0200

    Added comments

--- src/js/service/requestService.js
+++ src/js/service/requestService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const checkUser = () => {
-    return axios({
-        method: 'POST',
-        url: `${process.env.hostUrl || 'http://localhost:8081'}/api/user/count`,
-    })
+    // return axios({
+    //     method: 'POST',
+    //     url: `${process.env.hostUrl || 'http://localhost:8081'}/api/user/count`,
+    // })
 }
