7da50395e4c960c89d32b47d47cdc67938f678ab
Maksym Kuziv <mkuziv@hubspot.com>
2022-11-13 00:58:23 +0200

    Refactored code

--- src/js/components/AddPortfolio.js
+++ src/js/components/AddPortfolio.js
@@ -5,7 +5,7 @@ import '../../css/portfolio.css';
 const AddPortfolio = () => {
     return (
         <div>
-            <div className='back-non projects__project w-700 margin__left em-05'>
+            <div className='back-non projects__project w-704 margin__left em-05'>
                 <div className='text'>
                     <div className='add__project d-flex'>
                         <span className='add__project__text Gilroy-500'>Add my project</span>
